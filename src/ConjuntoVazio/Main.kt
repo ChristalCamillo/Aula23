d16da062bf46dbf108ebb92b20259e1f74aad85b
ChristalCamillo <christal.b.camillo@gmail.com>
2022-05-06 16:25:55 -0300

    conjuntovazio

---
+++ src/ConjuntoVazio/Main.kt
@@ -0,0 +1,19 @@
+package ConjuntoVazio
+
+
+// com o SET não podemos adicionar valores repetidos
+fun main() {
+
+    val vazio = mutableSetOf<Int>()
+    vazio.run {
+        add(1)
+        add(5)
+        add(5)
+        add(6)
+        add(7)
+        add(8)
+        add(8)
+        add(8)
+        println(this)
+    }
+}
